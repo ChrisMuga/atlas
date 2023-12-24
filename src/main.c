5f93fe3b34a2d608ab308314511803ef1692dbe0
Chris Muga <ChrisMuga94@Gmail.com>
2023-12-24 21:23:20 +0300

    misc

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 
 int main(void) {
     printf("Atlas: \n");
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 450, "Atlas");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Atlas", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(WHITE);
+            DrawText("Atlas", 190, 200, 20, BLACK);
         EndDrawing();
     }
 
